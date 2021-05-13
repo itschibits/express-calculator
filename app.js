a26be2ed9b4eeec6b13a9e01780435261d04d18a
S T <itschibits@gmail.com>
2021-05-12 20:01:12 -0400

    created calculator

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 /** Simple demo Express app. */
 
 const express = require("express");
+const stats = require("./stats.js");
+const utils = require("./utils");
 const app = express();
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean/", function(req, res) {
+  let intNums = utils.convertStrNums(req.query.nums)
+  let mean = stats.findMean(intNums);
+  return res.json({ operation: "mean", value: mean });
+
+});
 
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median/", function(req, res) {
+  let intNums = utils.convertStrNums(req.query.nums)
+  let median = stats.findMedian(intNums);
+  return res.json({ operation: "median", value: median });
+});
+
 
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode/", function(req, res) {
+  let intNums = utils.convertStrNums(req.query.nums)
+  let mode = stats.findMode(intNums);
+  return res.json({ operation: "mode", value: mode });
+});
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
--- utils.js
+++ utils.js
@@ -6,6 +6,19 @@ const { BadRequestError } = require("./expressError");
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  if (strNums === ""){
+    throw new BadRequestError("nums are required.")
+  }
+  let nums = strNums.split(",");
+  let intNums = []
+  for(let num in nums){
+    if (!parseInt(nums[num])){
+      throw new BadRequestError(`${nums[num]} is not a number`)
+    } else {
+      intNums.push(parseInt(nums[num]))
+    }
+  }
+  return intNums;
 }
 
 
